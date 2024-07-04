c5b484c6be45ae4bc754fce3686cc3d2032f783b
Seungwoo, Joo <thisis.joos@gmail.com>
2024-07-05 00:57:09 +0900

    feat: slack 알림을 비동기와 RabbitMQ 를 이용하여 보낸다

--- src/main/java/com/noritermap/api/global/slack/SlackAspect.java
+++ src/main/java/com/noritermap/api/global/slack/SlackAspect.java
@@ -2,22 +2,37 @@ package com.noritermap.api.global.slack;
 
 import com.noritermap.api.domain.facility.dto.FacilityResponseDto;
 import com.noritermap.api.domain.review.entity.Review;
+import com.noritermap.api.mq.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class SlackAspect {
-    private final SlackUtil slackUtil;
+
+    @Value("${rabbitmq.queue.name}")
+    private String QUEUE_NAME;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String EXCHANGE_NAME;
+
+    @Value("${rabbitmq.routing.key}")
+    private String ROUTING_KEY;
+
+    private final RabbitTemplate rabbitTemplate;
 
     // 리뷰 등록
     @AfterReturning(
             pointcut = "execution(* com.noritermap.api.domain.review.service.ReviewService.postReview(com.noritermap.api.domain.review.dto.ReviewRequestDto.ReviewPostRequestDto))",
             returning = "review"
     )
+    @Async
     public void afterRegisterReview(Review review){
         String message = String.format(
                 "facility_id : %s\n" +
@@ -27,7 +42,7 @@ public class SlackAspect {
                         "내용 : %s\n"
                 , review.getFacility().getId()+"", review.getFacility().getPfctNm(), review.getNickname(), review.getRating()+"", review.getContent());
 
-        slackUtil.sendSlackMessage(message, "#알림-리뷰중대");
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, new MessageDto("#알림-리뷰중대", message));
     }
 
     // 시설 상세정도 조회
@@ -38,7 +53,6 @@ public class SlackAspect {
     public void afterGetFacilityInfoBase(FacilityResponseDto.FacilityInfoBaseDto result){
         String message = String.format("facility id : %s, 시설 이름 : %s", result.getFacilityId(), result.getPfctNm());
 
-        slackUtil.sendSlackMessage(message, "#알림-상세정보조회");
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, new MessageDto("#알림-상세정보조회", message));
     }
-
 }
